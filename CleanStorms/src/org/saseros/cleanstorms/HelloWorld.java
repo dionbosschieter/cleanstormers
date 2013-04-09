7e651fc9bca990423628f3013f137e97836154c8
Dion Bosschieter <dionbosschieter@gmail.com>
2013-04-09 17:05:14 +0300

    Initializing project

---
+++ CleanStorms/src/org/saseros/cleanstorms/HelloWorld.java
@@ -0,0 +1,16 @@
+package org.saseros.cleanstorms;
+
+import lejos.nxt.*;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+		Button.waitForAnyPress();
+	}
+
+}
