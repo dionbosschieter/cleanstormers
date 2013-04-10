7dbbcd2f7227ec3c5fc1068c3f129400bbc94f55
Dion Bosschieter <dionbosschieter@gmail.com>
2013-04-10 13:40:15 +0300

    Created a driving example

---
+++ CleanStorms/src/org/saseros/cleanstorms/DrivingExample.java
@@ -0,0 +1,27 @@
+package org.saseros.cleanstorms;
+
+import lejos.nxt.*;
+
+public class DrivingExample {
+
+	/**
+	 * @param args
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+		
+		Motor.A.setSpeed(Motor.A.getMaxSpeed());
+		Motor.B.setSpeed(Motor.A.getMaxSpeed());
+		
+		Motor.A.forward();
+		Motor.B.forward();
+		
+		Thread.sleep(6000);
+		
+		Motor.B.stop();
+		Motor.A.stop();
+	}
+
+}
