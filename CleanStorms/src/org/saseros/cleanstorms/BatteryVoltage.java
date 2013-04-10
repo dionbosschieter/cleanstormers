377434eaff6b33d352b92e950a87f24d495b4411
Dion Bosschieter <dionbosschieter@gmail.com>
2013-04-10 16:28:32 +0300

    Created a battery example

---
+++ CleanStorms/src/org/saseros/cleanstorms/BatteryVoltage.java
@@ -0,0 +1,16 @@
+package org.saseros.cleanstorms;
+
+import lejos.nxt.*;
+
+public class BatteryVoltage {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Battery.getVoltage());
+		Button.waitForAnyPress();
+	}
+
+}
