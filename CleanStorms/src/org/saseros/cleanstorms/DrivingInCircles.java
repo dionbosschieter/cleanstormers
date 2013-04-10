7c4a1632d5c0564210db5d0d1c66fa41ad125ee2
Jose <Jose@Jose-PC.wlan.metropolia.fi>
2013-04-10 17:00:01 +0300

    Code that allows the robot to drive in circles(like a oval circuit)

---
+++ CleanStorms/src/org/saseros/cleanstorms/DrivingInCircles.java
@@ -0,0 +1,74 @@
+package org.saseros.cleanstorms;
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.Motor;
+
+
+public class DrivingInCircles {
+
+	/**
+	 * @param args
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		LCD.drawString("Program 1", 0,0);
+		Button.waitForAnyPress();
+		LCD.clear();
+		double maxSpeed = Motor.A.getMaxSpeed();
+		Motor.A.setSpeed(Motor.A.getMaxSpeed());
+		Motor.B.setSpeed(Motor.A.getMaxSpeed());
+		//FORWARD
+		Motor.A.forward();
+		Motor.B.forward();
+		LCD.drawString("FORWARD", 0,0);
+		Thread.sleep(3000);
+		/*Button.waitForAnyPress();*/
+		/*Turning right*/
+		Motor.A.stop();
+		Motor.B.forward();
+		LCD.drawString("TURNING RIGHT", 0, 0);
+		Thread.sleep(500);
+		
+		//FORWARD
+		
+		Motor.A.forward();
+		Motor.B.forward();
+		LCD.drawString("FORWARD", 0,0);
+		Thread.sleep(3000);
+		//TURNING RIGTH
+		Motor.A.stop();
+		Motor.B.forward();
+		LCD.drawString("TURNING RIGHT", 0, 0);
+		Thread.sleep(500);
+		//FORWARD
+		
+		Motor.A.forward();
+		Motor.B.forward();
+		LCD.drawString("FORWARD", 0,0);
+		Thread.sleep(3000);
+		//TURNING RIGTH
+		Motor.A.stop();
+		Motor.B.forward();
+		LCD.drawString("TURNING RIGHT", 0, 0);
+		Thread.sleep(500);
+		//FORWARD
+		
+		Motor.A.forward();
+		Motor.B.forward();
+		LCD.drawString("FORWARD", 0,0);
+		Thread.sleep(3000);
+		//TURNING RIGTH
+		Motor.A.stop();
+		Motor.B.forward();
+		LCD.drawString("TURNING RIGHT", 0, 0);
+		Thread.sleep(500);
+		
+		
+		/*Button.waitForAnyPress();*/
+		Motor.A.stop();
+		Motor.B.stop();
+		
+
+	}
+
+}
