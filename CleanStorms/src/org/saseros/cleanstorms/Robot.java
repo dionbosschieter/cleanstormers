7918f0ea39fd5c4d9c558c2e3ccb0fb85a21bee0
Pers <Pers@Pers-PC.wlan.metropolia.fi>
2013-04-16 15:53:12 +0300

    Fixed stack overflow in the recursive method isPathClear, and adjusted the turning angle of the Ultrasonic head

--- CleanStorms/src/org/saseros/cleanstorms/Robot.java
+++ CleanStorms/src/org/saseros/cleanstorms/Robot.java
@@ -9,28 +9,40 @@ public class Robot {
 
 	private Sensor sensor;
 	private int turn = -15;
+	private int recursiveDepth = 0;
 
 	public Robot(Sensor sensor) {
 		this.sensor = sensor;
 	}
 
 	public boolean isPathClear() {
+		recursiveDepth++;
+		if (recursiveDepth >= 246) {
+			atEndOfPathIsDetected();
+			System.out.println("Path not clear");
+			return false;
+		}
 		if (sensor.isObstacleDetected()) {
 			return isPathClear();
 		}
 		Motor.B.rotate(turn);
 		System.out.println("Surveying");
-		if (Motor.B.getTachoCount() <= -70) {
+		if (Motor.B.getTachoCount() <= -45) {
 			turn = +15;
 			return isPathClear();
-		} else if (Motor.B.getTachoCount() >= 70) {
+		} else if (Motor.B.getTachoCount() >= 45) {
 			System.out.println("Done Surveying!");
-			turn = -1;
-			moveUltrasonicSensorToDefaultPosition();
+			atEndOfPathIsDetected();
 			return true;
 		} else {
 			return isPathClear();
 		}
+	}
+
+	private void atEndOfPathIsDetected() {
+		moveUltrasonicSensorToDefaultPosition();
+		recursiveDepth = 0;
+		turn = -15;
 	}
 
 	private void moveUltrasonicSensorToDefaultPosition() {
@@ -43,8 +55,8 @@ public class Robot {
 	public static void main(String[] args) {
 		Sensor sensor = new Sensor(true, 20, 30);
 		Robot robot = new Robot(sensor);
-		robot.isPathClear();
-		System.out.println("Test complete");
+		System.out.println("Method returns: " + robot.isPathClear());
+		//robot.isPathClear();
 		Button.waitForAnyPress();
 		// while(!Button.ESCAPE.isDown()){
 
