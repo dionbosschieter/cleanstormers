9c8a7eff19e955ac6afc69535fc9811704bdf820
Dion Bosschieter <dionbosschieter@gmail.com>
2013-04-16 16:10:32 +0300

    Created the class Alarm to play a tune and show something on the screen this class can be used within other classes

--- CleanStorms/src/org/saseros/cleanstorms/test/Alarm.java
+++ CleanStorms/src/org/saseros/cleanstorms/test/Alarm.java
@@ -4,32 +4,15 @@ import lejos.nxt.*;
 
 public class Alarm {
 
-	/**
-	 * @param args
-	 */
-	private static final short[] note = {2349,115, 0,5, 1760,165, 0,35};
-
-	
-	public static void main(String[] args) throws Exception {
-		// TODO Make police-like sound
-		// TODO Make light emmit		
-		Sound.setVolume(50);
-		
-		
-		while(true){
-			
-			for(int i=0;i <note.length; i+=2) {
-				short w = note[i+1];
-				int n = note[i];
-				if (n != 0) {
-					Sound.playTone(n, w*10);
-				}
-				Thread.sleep(w*10);
-			}
-		}
-		
-		
-
+	public static void createAlarm(String error) {
+		LCD.clear();
+		LCD.drawString(error, 0, 0);
+		playAlarm();
 	}
-
+	
+	public static void playAlarm() {
+		Sound.setVolume(Sound.VOL_MAX);
+		Sound.systemSound(true, 3);
+		Button.waitForAnyPress();
+	}
 }
