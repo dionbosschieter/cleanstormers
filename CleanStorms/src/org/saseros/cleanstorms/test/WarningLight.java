63ebbcbae81a10c527758c748f4d9173707a54e6
Dion Bosschieter <dionbosschieter@gmail.com>
2013-04-16 15:48:23 +0300

    warning light thread created.

--- CleanStorms/src/org/saseros/cleanstorms/test/WarningLight.java
+++ CleanStorms/src/org/saseros/cleanstorms/test/WarningLight.java
@@ -2,17 +2,27 @@ package org.saseros.cleanstorms.test;
 
 import lejos.nxt.SensorPort;
 
-public class WarningLight {
+public class WarningLight implements Runnable {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws Exception {
-		// TODO
+	private int seconds;
+	
+	WarningLight(int countseconds) {
+		this.seconds = countseconds;
+	}
+	public void run() {
+		while(true) {
 			SensorPort.S4.setPowerType(2);
-			Thread.sleep(1000);
+			sleep();
 			SensorPort.S4.setPowerType(0);
-			Thread.sleep(1000);
+			sleep();
 		}
-
+	}
+	
+	private void sleep() {
+		try {
+			Thread.sleep(1000*seconds);
+		}	catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 }
