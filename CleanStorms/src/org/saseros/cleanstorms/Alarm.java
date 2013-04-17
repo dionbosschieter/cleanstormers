04dfe3ba0f8a254cab656e7f56f15227d89d673f
Dion Bosschieter <dionbosschieter@gmail.com>
2013-04-17 16:07:38 +0300

    Addedd javadoc

--- CleanStorms/src/org/saseros/cleanstorms/Alarm.java
+++ CleanStorms/src/org/saseros/cleanstorms/Alarm.java
@@ -4,12 +4,24 @@ import lejos.nxt.*;
 
 public class Alarm {
 
+	/**
+	 * Print string on the screen and play a beep
+	 * without waiting for someone pushing the button.
+	 * 
+	 * @param error
+	 */
 	public static void createAlarmSoft(String error) {
 		LCD.clear();
 		System.out.println(error);
 		playBeep();
 	}
 	
+	/**
+	 * Print string on the screen and play the alarm
+	 * And wait for a button to be pressed. 
+	 * 
+	 * @param error
+	 */
 	public static void createAlarmHard(String error) {
 		LCD.clear();
 		System.out.println(error);
@@ -17,17 +29,29 @@ public class Alarm {
 		Button.waitForAnyPress();
 		
 	}
+	
+	/**
+	 * Show a message on the screen
+	 * 
+	 * @param error
+	 */
 	public static void showMessage(String error) {
 		LCD.clear();
 		System.out.println(error);
 		Button.waitForAnyPress();
 	}
 	
+	/**
+	 * Set the volume to max and play ascending arpeggio
+	 */
 	public static void playAlarm() {
 		Sound.setVolume(Sound.VOL_MAX);
 		Sound.systemSound(true, 3);
 	}
 	
+	/**
+	 * Set the volume to max and play a beep
+	 */
 	public static void playBeep(){
 		Sound.setVolume(Sound.VOL_MAX);
 		Sound.beep();
